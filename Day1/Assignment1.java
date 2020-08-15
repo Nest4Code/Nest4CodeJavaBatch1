9264ccc7d743782bbab55820c465b35f4dd615d8
Nest4Code <69668840+Nest4Code@users.noreply.github.com>
2020-08-15 16:54:12 +0530

    Add files via upload

---
+++ Day1/Assignment1.java
@@ -0,0 +1,15 @@
+
+public class Assignment1 {
+	
+	public static void main(String[] args) {
+		// Input
+		float prod1 = 12;
+		float prod2 = 13;
+		float prod3 = 15;
+		float taxper = 10;
+		float finalSum = prod1 + prod2 + prod3;
+		float finalAmount = (taxper/100)*finalSum;
+		System.out.println("Final Amount"+finalAmount);
+	}
+	
+}
